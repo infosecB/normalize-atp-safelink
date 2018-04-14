2d1d93e19c80b754ae60b22928e73d132d0e8854
infosecB <infosecB@users.noreply.github.com>
2018-04-14 18:09:20 -0400

    Initial commit

---
+++ normalize_atpsafelink.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+import urllib
+
+
+def normalize_atpsafelink(safelink_url):
+    safelink_url = safelink_url.split("=")[1]
+    safelink_url = urllib.unquote(safelink_url).decode('utf8')
+    safelink_url = safelink_url.replace('&data', '')
+    return safelink_url
+
+
+def main():
+    print normalize_atpsafelink(args.url)
+
+
+if __name__ == "__main__":
+    args = ArgumentParser()
+    args.add_argument('-u', '--url', help="Enter the URL.", dest='url')
+    args = args.parse_args()
+    
+    main(args.url)
